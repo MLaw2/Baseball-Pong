e8d7e930bac300f9e3850d2ac2ef8a2fa96a3abd
Michael <l4wmich43l@gmail.com>
2024-02-15 22:56:23 -0800

    implemented basic input mechanic

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -11,9 +11,19 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float jump_strength;
     private UnityEngine.Vector3 move_vector;
     // Start is called before the first frame update
+
+    // Hit the ball with a raycast. Maybe do other casts for better flexibility (aim assist???)
+    private RaycastHit target;
+    [SerializeField] private GameObject player_cam;
+    private Transform origin;
+    [SerializeField] private float swing_distance;
+    [SerializeField] private float swing_force;
+
+
     void Start()
     {
         rbody = GetComponentInChildren<Rigidbody>();
+        origin = player_cam.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -21,12 +31,21 @@ public class PlayerInput : MonoBehaviour
     {
         move_vector = new UnityEngine.Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         UnityEngine.Vector3.Normalize(move_vector);
-        if (Input.GetButtonDown("Jump")) {
-            rbody.AddForce(UnityEngine.Vector3.up * jump_strength);
-        }
-        // if(Math.Abs(rbody.velocity.x) + Math.Abs(rbody.velocity.z) <= max_speed){
-        // rbody.AddForce(move_vector * move_speed, );
-        // }
         transform.Translate(move_vector * move_speed * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(origin.position, origin.TransformDirection(Vector3.forward), out target, swing_distance)) {
+                Debug.Log(target.transform.gameObject);
+                if (target.transform.CompareTag("Ball")) {
+                    Debug.DrawRay(origin.position, origin.TransformDirection(Vector3.forward) * target.distance, Color.yellow);
+                    Debug.Log("Did Hit");
+
+                    target.rigidbody.AddForce(origin.TransformDirection(Vector3.forward) * swing_force, ForceMode.VelocityChange);
+                }
+            }
+            else {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+        }
     }
 }
