fe0baf714a54a9b34b30988eb48a5808340f6037
Michael <l4wmich43l@gmail.com>
2024-02-20 14:59:51 -0800

    Built the skeleton of baseball hitting lol

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerInput : MonoBehaviour
 {
+    // for passing direction of the ball hit
+    enum Direction { Left, Right, Up }
+
+    // For tuning dual direction input test
+    [SerializeField] private float time_buffer;
+
     // Code from Physics Week
-    private Rigidbody rbody;
     [SerializeField] private float move_speed;
     // [SerializeField] private float max_speed;
     [SerializeField] private float jump_strength;
@@ -27,6 +34,8 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float charge_rate;
     private bool left_mouse_down;
     private bool right_mouse_down;
+    private float left_mouse_up;
+    private float right_mouse_up;
     private bool armed;
     private float charge_percent;
 
@@ -37,7 +46,6 @@ public class PlayerInput : MonoBehaviour
 
     void Start()
     {
-        rbody = GetComponentInChildren<Rigidbody>();
         origin = player_cam.GetComponent<Transform>();
     }
 
@@ -46,7 +54,7 @@ public class PlayerInput : MonoBehaviour
     {
         move_vector = new UnityEngine.Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));   // section for calculating the movement vector for the transform
         UnityEngine.Vector3.Normalize(move_vector);
-        transform.Translate(move_vector * move_speed * Time.deltaTime);
+        transform.Translate(move_speed * Time.deltaTime * move_vector);
 
         left_mouse_down = Input.GetMouseButton(0);
         right_mouse_down = Input.GetMouseButton(1);
@@ -56,25 +64,56 @@ public class PlayerInput : MonoBehaviour
             charge_percent += 1 * charge_rate;
         }
 
+        // if you release the mouse, measure the time it was released (for recording dual input)
+        if (Input.GetMouseButtonUp(0)) {
+            left_mouse_up = Time.time;
+        }
+        if (Input.GetMouseButtonUp(1)) {
+            right_mouse_up = Time.time;
+        }
+
         if (!left_mouse_down && !right_mouse_down && armed) {  // Section for calculating hitting the ball
-            swing();
+            if (Mathf.Abs(left_mouse_up - right_mouse_up) < 0.01) {
+                Swing(Direction.Up);
+            }
+            else if (left_mouse_up > right_mouse_up) {
+                Swing(Direction.Left);
+            }
+            else if (right_mouse_up < left_mouse_up) {
+                Swing(Direction.Right);
+            }
+            else {
+                Debug.Log("Either nothing is supposed to happen or we lose");
+            }
             armed = false;
+            charge_percent = 0;
         }
     }
 
     // TODO IMPLEMENT DIRECTIONAL HITS
-    void swing() {
+    void Swing(Direction dir) {
         if (Physics.Raycast(origin.position, origin.TransformDirection(Vector3.forward), out target, swing_distance)) {
-            Debug.Log(target.transform.gameObject);
             if (target.transform.CompareTag("Ball")) {
-                Debug.DrawRay(origin.position, origin.TransformDirection(Vector3.forward) * target.distance, Color.yellow);
-                Debug.Log("Did Hit");
+                Vector3 hit_vector;
 
-                target.rigidbody.AddForce(origin.TransformDirection(Vector3.forward) * swing_force * (charge_percent / 100), ForceMode.VelocityChange);
+                // Calculate the new vector for the ball
+                if (dir == Direction.Right) {
+                    Debug.Log("RIGHT");
+                    hit_vector = origin.TransformDirection(Vector3.forward + Vector3.right);
+                }
+                else if (dir == Direction.Left) {
+                    Debug.Log("LEFT");
+                    hit_vector = origin.TransformDirection(Vector3.forward + Vector3.left);
+                }
+                else { // dir is Up
+                    Debug.Log("UP");
+                    hit_vector = origin.TransformDirection(Vector3.forward + Vector3.up);
+                }
+
+                target.rigidbody.AddForce((charge_percent / 100) * swing_force * hit_vector, ForceMode.VelocityChange);
             }
         }
         else { // missed
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
             Debug.Log("Did not Hit");
         }
     }
